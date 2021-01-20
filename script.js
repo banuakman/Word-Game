a7a4fa4d9ffd305bc7e28186ac551ca7a19d297f
banuakman <banu@asterya.com>
2021-01-20 16:46:48 -0500

    add more pseudocode to script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// DEPENDENCIES (DOM Elements)==============================
+// DEPENDENCIES (DOM Elements)===============================
 // Array for blank letters 
 
 // STARTING DATA=============================================
-// Array of words to guess
+// Array of words to guess - Chosen word
 
+// FUNCTIONS=================================================
+// 
 
 // USER INTERRACTIONS========================================
 // The startGame function WHEN the Start button is clicked
 // The setTimer function starts and stops the timer
-// 
+
+// INITIALIZE================================================
+// Run the Start Function
